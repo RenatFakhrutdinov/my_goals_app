cf998dbf93edd4ca2b35a96c819c3a69d44bcb45
Renat <rfakhrutdinov1988@gmail.com>
2020-04-24 19:34:12 +0300

    feat: set portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'blocs/database_bloc/database_bloc_export.dart';
@@ -6,8 +7,12 @@ import 'blocs/simple_bloc_delegate.dart';
 import 'ui/main_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   BlocSupervisor.delegate = SimpleBlocDelegate();
-  runApp(MyApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
