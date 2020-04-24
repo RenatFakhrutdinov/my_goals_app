107cd4cf8bce2d2316dd0355805556b4cc3fc112
Renat <rfakhrutdinov1988@gmail.com>
2020-04-25 00:21:36 +0300

    feat: correct position of ui components

--- lib/ui/widgets/no_goals_widget.dart
+++ lib/ui/widgets/no_goals_widget.dart
@@ -6,49 +6,54 @@ import 'package:simple_animations/simple_animations.dart';
 class NoGoalsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Stack(
       children: <Widget>[
-        PlayAnimation<double>(
-          duration: Duration(milliseconds: 400),
-          tween: Tween(begin: 0.0, end: 80.0),
-          builder: (context, child, height) {
-            return PlayAnimation<double>(
-              duration: Duration(milliseconds: 1200),
-              delay: Duration(milliseconds: 500),
-              tween: Tween(
-                  begin: 2.0, end: MediaQuery.of(context).size.width - 32),
-              builder: (context, child, width) {
-                return Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.bottomRight,
-                        end: Alignment.topCenter,
-                        colors: [Colors.white, Colors.white54]),
-                    borderRadius: BorderRadius.all(Radius.circular(8)),
-                  ),
-                  width: width,
-                  height: height,
-                  child: TypewriterText(Strings.addGoal),
-                );
-              },
-            );
-          },
-        ),
-        PlayAnimation<double>(
-            duration: Duration(milliseconds: 1800),
-            delay: Duration(milliseconds: 1500),
-            tween: Tween(begin: 0.0, end: 1.0),
-            builder: (context, child, opacity) {
-              return Opacity(
-                opacity: opacity,
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  color: Colors.black,
-                ),
+        Align(
+          alignment: Alignment(0.0, -0.5),
+          child: PlayAnimation<double>(
+            duration: Duration(milliseconds: 400),
+            tween: Tween(begin: 0.0, end: 80.0),
+            builder: (context, child, height) {
+              return PlayAnimation<double>(
+                duration: Duration(milliseconds: 1200),
+                delay: Duration(milliseconds: 500),
+                tween: Tween(
+                    begin: 2.0, end: MediaQuery.of(context).size.width - 32),
+                builder: (context, child, width) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.bottomRight,
+                          end: Alignment.topCenter,
+                          colors: [Colors.white, Colors.white54]),
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                    width: width,
+                    height: height,
+                    child: TypewriterText(Strings.addGoal),
+                  );
+                },
               );
-            })
+            },
+          ),
+        ),
+        Align(
+          alignment: Alignment(0.0, 0.5),
+          child: PlayAnimation<double>(
+              duration: Duration(seconds: 3),
+              delay: Duration(milliseconds: 1500),
+              tween: Tween(begin: 0.0, end: 1.0),
+              builder: (context, child, opacity) {
+                return Opacity(
+                  opacity: opacity,
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.black,
+                  ),
+                );
+              }),
+        )
       ],
     );
   }
