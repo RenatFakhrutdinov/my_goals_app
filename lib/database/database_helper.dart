af5e2e125d6014e83c5450abfdc69c2fdcfb82e1
Renat <rfakhrutdinov1988@gmail.com>
2020-04-24 11:47:14 +0300

    feat: add CRUD methods

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,6 +1,86 @@
-class DatabaseHelper {
-  static final DatabaseHelper _instance = DatabaseHelper.internal();
-  factory DatabaseHelper() => _instance;
+import 'package:mygoalsapp/model/goal_item.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+class DatabaseHelper {
+  static const String tableGoals = "goals";
+  static const String columnId = "id";
+  static const String columnTitle = "title";
+  static const String columnDescription = "description";
+  static const String columnDate = "date";
+
+  static final DatabaseHelper db = DatabaseHelper.internal();
   DatabaseHelper.internal();
+
+  Database _database;
+
+  Future<Database> get database async {
+    print("Database Getter Called .");
+    if (_database != null) {
+      return _database;
+    }
+    _database = await createDatabase();
+    return _database;
+  }
+
+  Future<Database> createDatabase() async {
+    String dbPath = await getDatabasesPath();
+    return await openDatabase(
+      join(dbPath, 'goalsDB.db'),
+      version: 1,
+      onCreate: (Database database, version) async {
+        print("Database Created.");
+        await database.execute(
+          "CREATE TABLE $tableGoals "
+          "($columnId INTEGER PRIMARY KEY,"
+          "$columnTitle TEXT,"
+          "$columnDescription TEXT,"
+          "$columnDate TEXT"
+          ")",
+        );
+      },
+    );
+  }
+
+  Future<List<GoalItem>> getGoals() async {
+    final db = await database;
+    var goals = await db.query(
+      tableGoals,
+      columns: [columnId, columnTitle, columnDescription, columnDate],
+    );
+
+    List<GoalItem> goalList = List<GoalItem>();
+    goals.forEach((currentGoal) {
+      GoalItem item = GoalItem.fromMap(currentGoal);
+      goalList.add(item);
+    });
+    return goalList;
+  }
+
+  Future<GoalItem> insert(GoalItem item) async {
+    final db = await database;
+    item.id = await db.insert(tableGoals, item.toMap());
+    return item;
+  }
+
+  Future<int> update(GoalItem item) async {
+    final db = await database;
+
+    return await db.update(
+      tableGoals,
+      item.toMap(),
+      where: "id = ?",
+      whereArgs: [item.id],
+    );
+  }
+
+  Future<int> delete(int id) async {
+    final db = await database;
+
+    return await db.delete(
+      tableGoals,
+      where: "id = ?",
+      whereArgs: [id],
+    );
+  }
 }
--- lib/model/goal_item.dart
+++ lib/model/goal_item.dart
@@ -1,9 +1,11 @@
 class GoalItem {
+  int id;
   String title, description, date;
 
-  GoalItem(this.title, this.description, this.date);
+  GoalItem(this.id, this.title, this.description, this.date);
 
   GoalItem.fromMap(Map<String, dynamic> map) {
+    this.id = map["id"];
     this.title = map["title"];
     this.description = map["description"];
     this.date = map["date"];
@@ -11,6 +13,7 @@ class GoalItem {
 
   Map<String, dynamic> toMap() {
     Map<String, dynamic> map = Map<String, dynamic>();
+    map["id"] = id;
     map["title"] = title;
     map["dateCreated"] = description;
     map["date"] = date;
