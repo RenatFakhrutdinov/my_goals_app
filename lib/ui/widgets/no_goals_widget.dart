3144b22432b34f2400cb60b487a4f20f271f13cc
Renat <rfakhrutdinov1988@gmail.com>
2020-04-24 23:28:45 +0300

    feat: fade animation of container under TypewriterText

--- lib/ui/widgets/no_goals_widget.dart
+++ lib/ui/widgets/no_goals_widget.dart
@@ -6,33 +6,50 @@ import 'package:simple_animations/simple_animations.dart';
 class NoGoalsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: PlayAnimation<double>(
-        duration: Duration(milliseconds: 400),
-        tween: Tween(begin: 0.0, end: 80.0),
-        builder: (context, child, height) {
-          return PlayAnimation<double>(
-            duration: Duration(milliseconds: 1200),
-            delay: Duration(milliseconds: 500),
-            tween:
-                Tween(begin: 2.0, end: MediaQuery.of(context).size.width - 32),
-            builder: (context, child, width) {
-              return Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.bottomRight,
-                      end: Alignment.topCenter,
-                      colors: [Colors.white, Colors.white54]),
-                  borderRadius: BorderRadius.all(Radius.circular(8)),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        PlayAnimation<double>(
+          duration: Duration(milliseconds: 400),
+          tween: Tween(begin: 0.0, end: 80.0),
+          builder: (context, child, height) {
+            return PlayAnimation<double>(
+              duration: Duration(milliseconds: 1200),
+              delay: Duration(milliseconds: 500),
+              tween: Tween(
+                  begin: 2.0, end: MediaQuery.of(context).size.width - 32),
+              builder: (context, child, width) {
+                return Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.bottomRight,
+                        end: Alignment.topCenter,
+                        colors: [Colors.white, Colors.white54]),
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                  width: width,
+                  height: height,
+                  child: TypewriterText(Strings.addGoal),
+                );
+              },
+            );
+          },
+        ),
+        PlayAnimation<double>(
+            duration: Duration(milliseconds: 1800),
+            delay: Duration(milliseconds: 1500),
+            tween: Tween(begin: 0.0, end: 1.0),
+            builder: (context, child, opacity) {
+              return Opacity(
+                opacity: opacity,
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.black,
                 ),
-                width: width,
-                height: height,
-                child: TypewriterText(Strings.addGoal),
               );
-            },
-          );
-        },
-      ),
+            })
+      ],
     );
   }
 }
