4ef5e107de104d09d83577f8f8a4988d58fa43bb
Renat <rfakhrutdinov1988@gmail.com>
2020-04-24 16:00:53 +0300

    feat: add field of method

--- lib/ui/main_page.dart
+++ lib/ui/main_page.dart
@@ -1,8 +1,6 @@
 import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mygoalsapp/blocs/database_bloc/database_bloc_export.dart';
 import 'package:mygoalsapp/res/strings.dart';
 import 'package:mygoalsapp/ui/widgets/floating_action_button_widget.dart';
 import 'package:sky_background/sky_background.dart';
@@ -13,26 +11,29 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  DatabaseBloc _databaseBloc;
-
   @override
   void initState() {
     super.initState();
-    _databaseBloc = BlocProvider.of<DatabaseBloc>(context);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButtonWidget(),
+      floatingActionButton: FloatingActionButtonWidget(
+        method: () {},
+      ),
       body: DoubleBackToCloseApp(
-          snackBar: SnackBar(
-            content: Text(
-              Strings.doubleBack,
-              textAlign: TextAlign.center,
-            ),
+        snackBar: SnackBar(
+          content: Text(
+            Strings.doubleBack,
+            textAlign: TextAlign.center,
           ),
-          child: SkyBackground(child: Text('d'))),
+        ),
+        child: SkyBackground(
+            child: Center(
+          child: Text("test"),
+        )),
+      ),
     );
   }
 }
--- lib/ui/widgets/floating_action_button_widget.dart
+++ lib/ui/widgets/floating_action_button_widget.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class FloatingActionButtonWidget extends StatelessWidget {
+  final Function method;
+
+  const FloatingActionButtonWidget({Key key,@required this.method}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       elevation: 0.0,
       backgroundColor: Colors.white70,
-      onPressed: () {},
+      onPressed: method,
       child: Icon(Icons.add, color: Colors.blue[300]),
     );
   }
